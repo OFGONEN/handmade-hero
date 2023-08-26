2b8d0d8edfc79310e349fda671c9c1b6b7f40d64
ofgonen <ofgonen@gmail.com>
2023-08-27 01:54:38 +0300

    feat: Create simple window

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,14 +1,101 @@
 // OFGONEN - 2023
 #include <windows.h>
 
-// This Is called from C-Runtime Lib not Windows
-int CALLBACK WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow
-)
+LRESULT CALLBACK MainWindowCallback(
+    HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
-    MessageBox(0, "OFG",  "Omer", MB_OK | MB_ICONINFORMATION);
+    LRESULT Result = 0;
+    switch (Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("VM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("VM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("VM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("VM_ACTIVATEAPP\n");
+        } break;
+
+        default:
+        {
+            // OutputDebugStringA("default\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return Result;
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int ShowCode)
+{
+    WNDCLASS WindowClass = {}; // Initialize to ZERO
+
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    // WindowClass.hIcon;
+    // WindowClass.hCursor;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if(RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(
+            0, 
+            WindowClass.lpszClassName, 
+            "Handmade-Hero",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+            CW_USEDEFAULT, 
+            CW_USEDEFAULT, 
+            CW_USEDEFAULT, 
+            CW_USEDEFAULT, 
+            0, 
+            0, 
+            Instance, 
+            0); 
+        
+        if(WindowHandle)
+        {
+            MSG Message;
+            // for(;;)
+            while(1)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if(MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            //todo error logging
+        }
+    }
+    else
+    {
+        //todo error logging
+    }
+
+    // MessageBox(0, "OFG", "Omer", MB_OK | MB_ICONINFORMATION);
     return 0;
 }
